ccc0488e57abc8c03ca79262878913503c946cfb
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-18 17:35:21 -0500

    Co-Authors

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,28 @@
-function animal(){
+window.onload = function(){
  let form = document.querySelector("form");
- form.onsubmit = function(event){
+ form.onsubmit = function(event) {
   event.preventDefault();
-
+  document.getElementById("elephant").setAttribute("class","hidden");
+  document.getElementById("frog").setAttribute("class","hidden");
+  document.getElementById("jackalope").setAttribute("class","hidden");
+  document.getElementById("doh").setAttribute("class","hidden");
+  
   const animal1Input = document.getElementById("animal1Input").value;
 
     if (animal1Input === "elephant") {
-        document.getElementById("frog").removeAttribute("class");
-        document.getElementById("jackalope").removeAttribute("class");
-        document.getElementById("doh").removeAttribute("class");
+        document.getElementById("elephant").removeAttribute("class");
 
     } else if (animal1Input === "frog") {
-        document.getElementById("elephant").removeAttribute("class");
-        document.getElementById("jackalope").removeAttribute("class");
-        document.getElementById("doh").removeAttribute("class");
+        document.getElementById("frog").removeAttribute("class");
 
     } else if (animal1Input === "jackalope") {
-        document.getElementById("frog").removeAttribute("class");
-        document.getElementById("elephhant").removeAttribute("class");
-        document.getElementById("doh").removeAttribute("class");
+        document.getElementById("jackalope").removeAttribute("class");
 
     } else {
-        document.getElementById("frog").removeAttribute("class");
-        document.getElementById("jackalope").removeAttribute("class");
-        document.getElementById("elephant").removeAttribute("class");
+        document.getElementById("doh").removeAttribute("class");
     }
- }
+ } 
 
+  
+  
 }
